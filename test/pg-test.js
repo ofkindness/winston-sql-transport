b46ce798eed1a23da40c421df58ba82ce3acb043
ofkindness <andrei.tretyakov@gmail.com>
2017-09-06 15:33:31 +0300

    -Update .eslintrc, - Update dependencies

--- test/pg-test.js
+++ test/pg-test.js
@@ -14,11 +14,11 @@ vows.describe('pg-transport')
   .addBatch({
     'An instance of the Postgres Transport': transport(Universal, {
       client: 'pg',
-      conString: 'postgres://' + process.env.POSTGRES_USER +
-        ':' + process.env.POSTGRES_PASSWORD +
-        '@' + process.env.POSTGRES_HOST +
-        ':' + process.env.POSTGRES_PORT +
-        '/' + process.env.POSTGRES_DBNAME,
+      connectionString: `postgres://${process.env.PGUSER}\
+:${process.env.PGPASSWORD}\
+@${process.env.PGHOST}\
+:${process.env.PGPORT}\
+/${process.env.PGDATABASE}`,
       name: 'Postgres'
     })
   })
