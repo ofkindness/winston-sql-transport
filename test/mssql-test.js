20c13705b55073ff5e50222e83079c421483f100
Andrei Tretyakov <andrei.tretyakov@gmail.com>
2021-05-07 17:43:26 +0300

    Update dependencies for 3.* winston version

--- test/mssql-test.js
+++ test/mssql-test.js
@@ -5,47 +5,26 @@
  * @author Andrei Tretyakov <andrei.tretyakov@gmail.com>
  */
 
-const assert = require('assert');
-const { Logger } = require('winston');
-const { SQLTransport } = require('./../lib/winston-sql-transport');
-const transport = require('./transport.js');
-const vows = require('vows');
+const testSuite = require('abstract-winston-transport');
 
-const logger = new Logger({
-  transports: [
-    new SQLTransport({
-      client: 'mssql',
-      connection: {
-        user: process.env.MSSQL_USER,
-        password: process.env.MSSQL_PASSWORD,
-        server: process.env.MSSQL_HOST,
-        database: process.env.MSSQL_DB
-      },
-      name: 'SQLTransport',
-      pool: {
-        min: 0,
-        max: 10
-      },
-      tableName: 'winston_logs'
-    })
-  ]
+const { SQLTransport } = require('../lib/winston-sql-transport');
+
+testSuite({
+  name: 'MSSQL',
+  Transport: SQLTransport,
+  construct: {
+    client: 'mssql',
+    connection: {
+      user: process.env.MSSQL_USER,
+      password: process.env.MSSQL_PASSWORD,
+      server: process.env.MSSQL_HOST,
+      database: process.env.MSSQL_DB
+    },
+    name: 'MSSQL',
+    pool: {
+      min: 0,
+      max: 10
+    },
+    tableName: 'winston_logs'
+  }
 });
-
-vows
-  .describe('winston-sql-transport')
-  .addBatch({
-    'An instance of the SQL Transport - SQL Server': {
-      topic: function topic() {
-        const { callback } = this;
-        logger.transports.SQLTransport.init().then(() => callback(null, true));
-      },
-      'should create table': (err, result) => {
-        assert.isNull(err);
-        assert.ok(result === true);
-      }
-    }
-  })
-  .addBatch({
-    'An instance of the SQL Transport - SQL Server': transport(logger, logger.transports.SQLTransport)
-  })
-  .export(module);
--- test/mysql-test.js
+++ test/mysql-test.js
@@ -5,48 +5,27 @@
  * @author Andrei Tretyakov <andrei.tretyakov@gmail.com>
  */
 
-const assert = require('assert');
-const { Logger } = require('winston');
-const { SQLTransport } = require('./../lib/winston-sql-transport');
-const transport = require('./transport.js');
-const vows = require('vows');
+const testSuite = require('abstract-winston-transport');
 
-const logger = new Logger({
-  transports: [
-    new SQLTransport({
-      client: 'mysql',
-      connection: {
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE
-      },
-      name: 'MySQL',
-      pool: {
-        min: 0,
-        max: 10
-      },
-      tableName: 'winston_logs'
-    })
-  ]
+const { SQLTransport } = require('../lib/winston-sql-transport');
+
+testSuite({
+  name: 'MySQL',
+  Transport: SQLTransport,
+  construct: {
+    client: 'mysql',
+    connection: {
+      host: process.env.MYSQL_HOST,
+      port: process.env.MYSQL_PORT,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE
+    },
+    name: 'MySQL',
+    pool: {
+      min: 0,
+      max: 10
+    },
+    tableName: 'winston_logs'
+  }
 });
-
-vows
-  .describe('winston-sql-transport')
-  .addBatch({
-    'An instance of the SQL Transport - MySQL': {
-      topic: function topic() {
-        const { callback } = this;
-        logger.transports.SQLTransport.init().then(() => callback(null, true));
-      },
-      'should create table': (err, result) => {
-        assert.isNull(err);
-        assert.ok(result === true);
-      }
-    }
-  })
-  .addBatch({
-    'An instance of the SQL Transport - MySQL': transport(logger, logger.transports.SQLTransport)
-  })
-  .export(module);
