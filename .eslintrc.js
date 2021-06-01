eb6b959d63621c04c0c6cb83c59adcc8ea3a745d
Andrei Tretyakov <andrei.tretyakov@gmail.com>
2021-06-01 21:16:20 +0300

    Update packages

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-param-reassign': ['error', { props: false }],
+  },
+  plugins: ['prettier'],
+};
